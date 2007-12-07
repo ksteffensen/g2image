592b4001ac8fc4424b34cca9c0381de0aaa3bdbb
Kirk Steffensen <kirk@steffensenfamily.com>
2007-12-07 18:31:36 +0000

    Testing new way to get album child items with all of the available versions (fullsize, thumbnails, and resizes).

--- g2image.php
+++ g2image.php
@@ -578,7 +578,10 @@ function g2ic_make_html_select($name,$options,$onchange=null) {
 
 	return $html;
 }
-list ($ret, $childItemIds, $thumbnailImageItems, $fullsizeImageItems, $resizeImageItems) = $g2obj->getAllChildImageItemsForAlbum($g2ic_options['current_album']);
+list ($ret, $childItemIds, $thumbnailImageItems, $fullsizeImageItems, $resizeImageItems) = $g2obj->fetchAllChildImageItemsForAlbum($g2ic_options['current_album']);
+if ($ret) {
+	echo $ret->getAsHtml();
+}
 echo debug::show($childItemIds, 'childItemIds');
 echo debug::show($thumbnailImageItems, 'thumbnailImageItems');
 echo debug::show($fullsizeImageItems, 'fullsizeImageItems');
