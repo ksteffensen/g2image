926fa5cad138e4ae35c0d6efc31349ccf7659948
Kirk Steffensen <kirk@steffensenfamily.com>
2007-12-14 03:08:13 +0000

    Removed all of the hidden input values, except image_id, by dumping the PHP arrays to javascript arrays.

--- header.class.php
+++ header.class.php
@@ -4,32 +4,31 @@ class g2ic_header {
 	
 	var $html = '';
 	
-	/* *************************
-	 * for PHP4 compatibility
-	 * there should be a function called like the class
-	 * *************************
+	/**
+	 * Enter description here...
+	 *
+	 * @param unknown_type $options
+	 * @return g2ic_header
 	 */
-	function g2ic_header($options){
-		__construct($options);
+	function g2ic_header($options, $g2obj){
+		__construct($options, $g2obj);
 	}
 
-
-	/* *************************
-	 * Constuct the head HTML
-	 * 
-	 * @return an object
-	 * *************************
+	/**
+	 * Enter description here...
+	 *
+	 * @param unknown_type $options
 	 */
-	 /*public*/ function __construct($options){
+	function __construct($options, $g2obj){
 		$this->html = 
 '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <title>' . T_('Gallery2 Image Chooser') . '</title>
-    <link rel="stylesheet" href="css/g2image.css" type="text/css" />
-    <link rel="stylesheet" href="css/dtree.css" type="text/css" />
-    <link rel="stylesheet" href="css/slimbox.css" type="text/css" media="screen" />
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />';
+	<title>' . T_('Gallery2 Image Chooser') . '</title>
+	<link rel="stylesheet" href="css/g2image.css" type="text/css" />
+	<link rel="stylesheet" href="css/dtree.css" type="text/css" />
+	<link rel="stylesheet" href="css/slimbox.css" type="text/css" media="screen" />
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />';
 		if($options['tinymce'] && $options['wpg2_valid']) {
 			$this->html .= "    <script language='javascript' type='text/javascript' src='../../../../wp-includes/js/tinymce/tiny_mce_popup.js'></script>\n";
 		}
@@ -37,11 +36,11 @@ class g2ic_header {
 			$this->html .= "    <script language='javascript' type='text/javascript' src='../../tiny_mce_popup.js'></script>\n";
 		}
 		$this->html .= 
-'    <script language="javascript" type="text/javascript" src="jscripts/functions.js"></script>
-    <script language="javascript" type="text/javascript" src="jscripts/dtree.js"></script>
-    <script language="javascript" type="text/javascript" src="jscripts/mootools.js"></script>
-    <script language="javascript" type="text/javascript" src="jscripts/slimbox.js"></script>
-    <script type="text/javascript">
+'	<script language="javascript" type="text/javascript" src="jscripts/functions.js"></script>
+	<script language="javascript" type="text/javascript" src="jscripts/dtree.js"></script>
+	<script language="javascript" type="text/javascript" src="jscripts/mootools.js"></script>
+	<script language="javascript" type="text/javascript" src="jscripts/slimbox.js"></script>
+	<script type="text/javascript">
 ';
 		if ($options['album_modules']) {
 			foreach($options['album_modules'] as $moduleName){
@@ -54,10 +53,23 @@ class g2ic_header {
 			}
 		}
 		$this->html .= 
-'    </script>
-    <script language="javascript" type="text/javascript">
-    <!--
-	function insertAlbum(){
+'	</script>
+	<script language="javascript" type="text/javascript">
+	<!--
+	function g2icInsert(insertType){
+';
+		$this->html .= PhpArrayToJsObject($options, 'options');
+		$this->html .= PhpArrayToJsObject($g2obj->album, 'album');
+		$this->html .= PhpArrayToJsObject($g2obj->dataItems, 'dataItems');
+		$this->html .= 
+'		if(insertType == "album") {
+			insertAlbum(album, options);
+		}
+		else {
+			insertItems(dataItems, album, options);
+		}
+	}
+	function insertAlbum(album, options){
 		var obj = document.forms[0];
 		var htmlCode = \'\';
 
@@ -66,15 +78,17 @@ class g2ic_header {
 			var imageObj = {}; // new Object()
 
 			// Fixed core variables
-			imageObj.current_album = obj.current_album.value;
-			imageObj.album_name = obj.album_name.value;
-			imageObj.album_url = obj.album_url.value;
-			imageObj.album_summary = obj.album_summary.value;
-			imageObj.album_thumbnail = obj.album_thumbnail.value;
-			imageObj.album_thumbw = obj.album_thumbw.value;
-			imageObj.album_thumbh = obj.album_thumbh.value;
+			imageObj.current_album = album.id;
+			imageObj.album_name = album.title;
+			imageObj.album_url = album.image_url;
+			imageObj.album_summary = album.summary;
+			if(album.thumbnail_id){
+				imageObj.album_thumbnail = album.imageVersions[album.thumbnail_id]["url"]["image"];
+				imageObj.album_thumbw = album.imageVersions[album.thumbnail_id]["width"];
+				imageObj.album_thumbh = album.imageVersions[album.thumbnail_id]["height"];
+			}
 			imageObj.album_alignment = obj.album_alignment.value;
-			imageObj.class_mode = obj.class_mode.value;
+			imageObj.class_mode = options.class_mode;
 
 			// Module inserted variables
 			// Album modules
@@ -92,10 +106,10 @@ class g2ic_header {
 			htmlCode += \'Error\';
 		}
 
-		insertHtml(htmlCode,obj);
+		insertHtml(htmlCode, options.form, options.field);
 	}
 
-	function insertItems(){
+	function insertItems(items, album, options){
 		var obj = document.forms[0];
 		var htmlCode = \'\';
 		var imgtitle = \'\';
@@ -117,27 +131,11 @@ class g2ic_header {
 			loop = obj.images.length;
 			for (var i=0;i<loop;i++) {
 				image_id[i] = obj.image_id[i].value;
-				item_title[i] = obj.item_title[i].value;
-				item_summary[i] = obj.item_summary[i].value;
-				item_description[i] = obj.item_description[i].value
-				image_url[i] = obj.image_url[i].value;
-				fullsize_img[i] = obj.fullsize_img[i].value;
-				thumbnail_img[i] = obj.thumbnail_img[i].value;
-				thumbw[i] = obj.thumbw[i].value;
-				thumbh[i] = obj.thumbh[i].value;
 			}
 		}
 		else {
 			loop = 1;
 			image_id[0] = obj.image_id.value;
-			item_title[0] = obj.item_title.value;
-			item_summary[0] = obj.item_summary.value;
-			item_description[0] = obj.item_description.value
-			image_url[0] = obj.image_url.value;
-			thumbnail_img[0] = obj.thumbnail_img.value;
-			fullsize_img[0] = obj.fullsize_img.value;
-			thumbw[0] = obj.thumbw.value;
-			thumbh[0] = obj.thumbh.value;
 		}
 
 		//let\'s generate HTML code according to selected insert option
@@ -155,23 +153,22 @@ class g2ic_header {
 					// Fixed core variables
 					imageObj.pos = i;
 					imageObj.image_id = image_id[i];
-					imageObj.image_url = image_url[i];
-					imageObj.album_url = obj.album_url.value;
-					imageObj.fullsize_img = fullsize_img[i];
-					imageObj.thumbnail_img = thumbnail_img[i];
-					imageObj.thumbw = thumbw[i];
-					imageObj.thumbh = thumbh[i];
+					id = image_id[i];
+					imageObj.image_url = items[id].image_url;
+					imageObj.album_url = album.image_url;
+					imageObj.fullsize_img = fullsize_img[id];
+					imageObj.thumbnail_img = items[id].imageVersions[items[id].thumbnail_id]["url"]["image"];
+					imageObj.thumbw = items[id].imageVersions[items[id].thumbnail_id]["width"];
+					imageObj.thumbh = items[id].imageVersions[items[id].thumbnail_id]["height"];
 					imageObj.w = false; 			// to be done
 					imageObj.h =  false; 			// to be done
-					imageObj.item_title = item_title[i];
-					imageObj.item_summary = item_summary[i];
-					imageObj.item_description = item_description[i];
-					imageObj.album_id =  false; 		// to be done
-					imageObj.keywords =  false; 		// to be done
-					imageObj.derivatives =  false; 		// to be done
-					imageObj.siblings =  false; 		// to be done
+					imageObj.item_title = items[id].title;
+					imageObj.item_summary = items[id].summary;
+					imageObj.item_description = items[id].description;
+					imageObj.album_id =  album.id;
+					imageObj.keywords =  items[id].keywords;
 					imageObj.alignment = obj.alignment.value;
-					imageObj.class_mode = obj.class_mode.value;
+					imageObj.class_mode = options.class_mode;
 
 					// Module inserted variables
 					// Image modules
@@ -190,7 +187,7 @@ class g2ic_header {
 				}
 			}
 		}
-		insertHtml(htmlCode,obj);
+		insertHtml(htmlCode, options.form, options.field);
 	}
     // -->
     </script>
