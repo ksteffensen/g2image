14683dee16c187c69e72f2939dc94f88c2a15df1
Kirk Steffensen <kirk@steffensenfamily.com>
2007-12-19 16:13:59 +0000

    Troubleshooting g2 init in TinyMCE

--- g2image/init.php
+++ g2image/init.php
@@ -21,6 +21,8 @@ $g2ic_options['current_page'] = 1;
 $g2ic_options['wpg2_valid'] = FALSE;
 $g2ic_options['wp_rel_path'] = '';
 echo $_SERVER['PHP_SELF'] . '<br />';
+echo dirname($_SERVER['PHP_SELF']) . '<br />';
+echo substr_count(dirname($_SERVER['PHP_SELF']), "/") . '<br />';
 $g2ic_options['base_path'] = str_repeat("../", substr_count(dirname($_SERVER['PHP_SELF']), "/"));
 
 // Convert the variables from config.php to $g2ic_options array items.
