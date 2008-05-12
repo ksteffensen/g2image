77696a2b6ae2ca00ab86ce85fdc918c99054c2bf
Kirk Steffensen <kirk@steffensenfamily.com>
2008-05-12 12:46:16 +0000

    Importing historical files.

---
+++ langs/es.js
@@ -0,0 +1,15 @@
+// Spanish lang variables - Version 2.2
+
+// If you translate this file to your own language, please zip it and e-mail
+// it to blogger (at sign) steffensenfamily (dot) com and I'll make it available
+// for others and include it in the next version.  Thanks!
+
+tinyMCE.addToLang('g2image',{
+button_title : 'Inserte una imagen Gallery 2',
+wpg2_album_photo_name : 'Nombre de Album/Foto de Gallery2:',
+wpg2_popup_title : 'WPG2 Editor de Tags',
+wpg2_update : 'Actualizar Tag WPG2:',
+wpg2id_id : 'Item ID Gallery2:',
+wpg2id_popup_title : 'WPG2ID Editor de Tags',
+wpg2id_update : 'Actualizar Tag WPG2ID:'
+});
