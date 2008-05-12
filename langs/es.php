7491bd43514351b0bd11c23233c47cf68e537a34
Kirk Steffensen <kirk@steffensenfamily.com>
2008-05-12 12:58:44 +0000

    Importing historical files.

---
+++ langs/es.php
@@ -0,0 +1,95 @@
+<?php
+
+// Gallery2 Image Chooser pop-up window Spanish lang variables - Version 2.2
+
+// If you translate this file to your own language, please zip it and e-mail
+// it to blogger (at sign) steffensenfamily (dot) com and I'll make it available
+// for others and include it in the next version.  Thanks!
+
+$g2ic_lang = array(
+'about' => 'Acerca de G2Image',
+'alignment_class' => 'Alignment Class:',
+'alignment_legend' => 'Alignment Class (CSS debe soportar las clases g2image para ser efectivo):',
+'click_advanced' => 'Abrir el Menú de Opciones Avanzadas',
+'click_image' => 'Click en una imagen para opciones de imagen.',
+'click_one_click' => 'Insertar ajuste de uso por defecto',
+'click_options' => 'Escoja el resultado de hacer click en una imagen:',
+'current_album' => 'Álbum actual:',
+'custom_url' => 'URL propia:',
+'description' => 'Descripción:',
+'description_legend' => 'Texto para la imagen alternativa or texto de link:',
+'display_legend' => 'Opciones de Visualización',
+'drupal_album_legend' => 'Inserte un etiqueta de filtro de Drupal G2 para el álbum actual:',
+'drupal_g2_filter' => 'Filtro Drupal Gallery2',
+'empty_album' => 'No hay fotos en este álbum.<br /><br />Por favor, escoja otro álbum de las opciones de navegación de abajo.',
+'filename' => 'Nombre de archivo: ',
+'filenames' => 'Nombres de Archivos',
+'g2_directory_error' => 'No se puede abrir la carpeta de datos de Gallery2.',
+'g2_embedded_error' => '<h3>Error fatal de Gallery2: No se pueden activar las funciones de Gallery2 embebido.</h3><br />Para los usuarios de WordPress, instalar el plugin WPG2
+(situado en <a href="http://wpg2.ozgreg.com/">http://wpg2.ozgreg.com/</a>) o compruebe la ruta del gallery2 en el popup de opciones del admin panel de G2Image.<br /><br />Para otras plataformas, por favor, verifique la ruta a Gallery2 en config.php.',
+'g2_entities_by_id_error' => '<h3>Error Fatal Gallery2: No se puede obtener la info de imagen para este ID de Gallery2.</h3><br />Aquí está el error de G2: ',
+'g2_fatal_error' => '<h3>Error Fatala Gallery2:</h3><br />Aquí está el error de G2: ',
+'g2_id_by_path_error' => '<br /><h3>Error Gallery2: No se puede obtener el ID Gallery2 para la ruta solicitada.</h3><br />Esto es, normalmente, por un archivo
+
+ o carpeta en la estructura de datos de Gallery2, pero que no está registrado en Gallery2 como parte de la estructura de su álbum. El resto de funcionalidades de esta página funcionarán.<br /><br />Aquí está la parte no válida: ',
+'g2_id_not_found_error' => '<h3>Error Fatal Gallery2: No se puede obtener el ID Gallery2 para este archivo.</h3>Aquí está el error de G2: ',
+'g2_image_title' => 'Título Gallery2:',
+'go' => 'Adelante',
+'how_insert' => 'Como insertar:',
+'image_title' => 'Título: ',
+'img_float_left' => 'Flotante Izquierda',
+'img_float_right' => 'Flotante Derecha',
+'img_centered' => 'Centrado',
+'img_none' => 'Ninguna',
+'img_normal' => 'Normal',
+'insert' => 'Insertar',
+'insert_legend' => 'Pulse para insertar imagen:',
+'last_modification_new' => 'Última modificación (Más nuevos, primero)',
+'last_modification_old' => 'Última Modificación (Mñas viejos, primero)',
+'link_album' => 'Texto link para álbum padre',
+'link_image' => 'Texto link para imagen',
+'name_a_to_z' => 'Nombre Archivo (A-z)',
+'name_z_to_a' => 'Nombre Archivo (z-A)',
+'page' => 'Página:',
+'page_navigation' => 'Navigación Pags.:',
+'photo_albums' => 'Álbumes de Fotos',
+'redraw' => 'Redibujar',
+'sort_error' => 'Error ordenando por',
+'sorted_by' => 'Ordenado por:',
+'subalbums' => 'Subálbumes:',
+'thumbnails' => 'Miniaturas',
+'thumbnail_album' => 'Miniatura con link a álbum padre',
+'thumbnail_custom_url' => 'Miniatura con link a una URL (del textbox de abajo)',
+'thumbnail_image' => 'Miniatura con link a imagen',
+'thumbnail_only' => 'Sólo Miniatura only - sin link',
+'thumbnails_per_page' => 'Por Página:',
+'title' => 'Selector de Imágenes de Gallery2',
+'title_a_to_z' => 'Título Gallery2 (A-z)',
+'title_z_to_a' => 'Título Gallery2 (z-A)',
+'url' => 'URL:',
+'wpg2_album_legend' => 'Inserte un tag WPG2 para el álbum actual:',
+'wpg2_configuration_error' => 'Error de Configuración:<br />WPG2 está presente y activo, pero no está validado.<br />Por favor, configure y valide WPG2 en
+
+panel de administración de WordPress.<br />Instrucciones en http://www.ozgreg.com.',
+'wpg2_link_album' => 'Link de texto al álbum padre (HTML)',
+'wpg2_link_image' => 'Link de texto a imagen (HTML)',
+'wpg2_tag_image' => 'tag WPG2 de imagen',
+'wpg2_thumbnail_album' => 'Miniatura con link a álbum padre (HTML)',
+'wpg2_thumbnail_custom_url' => 'Miniatura con linka URL propia (Del textbox de abajo) (HTML)',
+'wpg2_thumbnail_image' => 'Miniatura con link a imagen (HTML)',
+'wpg2_thumbnail_only' => 'Sólo miniatura - sin link (HTML)',
+//
+// New for version 2.2
+//
+'fullsize_image' => 'Fullsized image with link to Gallery page for image',
+'fullsize_only' => 'Fullsized image only - no link',
+'image_size' => 'Image Size',
+'image_size_instructions' => 'Leave size blank the current default size of ',
+'orignination_time_new' => 'Origination Time (Newest First)',
+'orignination_time_old' => 'Origination Time (Oldest First)',
+'thumbnail_lightbox' => 'Thumbnail with LightBox link to Fullsized Image',
+'titles' => 'Titles',
+'wpg2_fullsize_image' => 'Fullsized image with link to Gallery page for image (HTML)',
+'wpg2_fullsize_only' => 'Fullsized image only - no link (HTML)',
+'wpg2_thumbnail_lightbox' => 'Thumbnail with LightBox link to Fullsized Image (HTML)')
+?>
