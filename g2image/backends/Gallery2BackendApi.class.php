823bf83359f1f64b516b9077938b28bb44259d0f
Kirk Steffensen <kirk@steffensenfamily.com>
2008-05-13 00:38:38 +0000

    Troubleshooting entity type problem on WPG2Next

--- g2image/backends/Gallery2BackendApi.class.php
+++ g2image/backends/Gallery2BackendApi.class.php
@@ -779,7 +779,7 @@ class Gallery2BackendApi{
 				}
 			}
 			print $id;
-			print_r($fullsizes[$id]);
+			print_r($fullsizes);
 			$fullsize_entity_type = $fullsizes[$id]->getEntityType();
 			if (!empty($fullsizes[$id]) && (($fullsize_entity_type == 'GalleryPhotoItem') || $fullsize_entity_type == 'GalleryDerivativeImage')) {
 				$image_version = $fullsizes[$id];
